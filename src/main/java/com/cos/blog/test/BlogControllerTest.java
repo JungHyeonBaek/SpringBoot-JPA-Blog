aa571fb1477b6e43a99bf419725e3f05f01b54ce
JungHyeonBaek <trefoil77@gmail.com>
2023-08-11 22:54:09 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String getHello() {
+		return "<h1>Hello My First Blog</h1>";
+	}
+}
